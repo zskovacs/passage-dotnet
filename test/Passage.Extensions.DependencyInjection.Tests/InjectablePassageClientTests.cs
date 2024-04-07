21248d32f1449bfd71fb3f6290c11fd193092f6f
Zsolt Kovács <zs.kovacs@proton.me>
2024-04-07 22:00:13 +0200

    Add some unit tests

---
+++ test/Passage.Extensions.DependencyInjection.Tests/InjectablePassageClientTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq;
+using Passage.Configuration;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Passage.Extensions.DependencyInjection.Tests;
+
+
+public class InjectablePassageClientTests
+{
+    [Fact]
+    public void Constructor_Should_Setup_HttpClient_Headers()
+    {
+        // Arrange
+        var apiKey = "test_api_key";
+        var appId = "test_app_id";
+        
+        var config = new PassageConfig { ApiKey = apiKey, AppId = appId };
+        var mockOptions = new Mock<IOptions<PassageConfig>>();
+        mockOptions.Setup(o => o.Value).Returns(config);
+
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+        // Act
+        var injectablePassageClient = new InjectablePassageClient(mockOptions.Object, httpClient);
+
+        // Assert
+        httpClient.DefaultRequestHeaders.Authorization.Should().BeEquivalentTo(new AuthenticationHeaderValue("Bearer", apiKey));
+        httpClient.DefaultRequestHeaders.GetValues("Passage-Version").Should().ContainSingle()
+            .Which.Should().Be(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+    }
+}
---
+++ test/Passage.Tests/UserTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Moq;
+using Moq.Contrib.HttpClient;
+using Passage.Configuration;
+using Passage.Exceptions;
+using Passage.OpenApi;
+using System.Net;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Passage.Tests;
+
+public class UserTests
+{
+    [Fact]
+    public void Initialization_NoConfig_Should_ThrowException()
+    {
+        // Arrange
+
+        // Act
+        var act = () => { _ = new User(null); };
+
+        // Assert
+        act.Should().Throw<PassageException>().WithMessage(Errors.Config.MissingAppId);
+    }
+
+    [Fact]
+    public void Initialization_NoAppId_Should_ThrowException()
+    {
+        // Arrange
+        var config = new PassageConfig();
+
+        // Act
+        var act = () => { _ = new User(config); };
+
+        // Assert
+        act.Should().Throw<PassageException>().WithMessage(Errors.Config.MissingAppId);
+    }
+
+    [Fact]
+    public void Initialization_Ok()
+    {
+        // Arrange
+        var config = new PassageConfig() { AppId = "xxx" };
+
+        // Act
+        var act = () => { _ = new User(config); };
+
+        // Assert
+        act.Should().NotThrow();
+    }
+}
